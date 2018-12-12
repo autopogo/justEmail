222792bf0b5955f7ccba269a6e377612ab048530
Andrew Pikul <ajp@autopogo.com>
2018-12-11 22:52:51 -0500

    improved error handling- retries connect 3 times if timeout to relay

--- email.go
+++ email.go
@@ -3,9 +3,15 @@ package justEmail
 import (
 	"crypto/tls"
 	"fmt"
-	"log"
 	"net/smtp"
 	"strings"
+  log "github.com/autopogo/justLogging"
+	"errors"
+)
+
+var (
+ ErrServerUnavailable = errors.New("justEmail: Couldn't connect to the relay server")
+ ErrBadConfig = errors.New("justEmail: Tried to initialize SMTP w/o required parameters set")
 )
 
 // The Mail struct allows you to build emails easily
@@ -46,15 +52,17 @@ type SmtpServer struct {
 }
 
 // ServerName just concatenates the hostname and portnumber
-func (s *SmtpServer) ServerName() string {
+func (s *SmtpServer) serverName() string {
 	return s.Host + ":" + s.Port
 }
 
 // StartServer just connects to an email server
-func (s *SmtpServer) StartServer() {
+func (s *SmtpServer) StartServer() error {
 	if s.Host == "" || s.Port == "" || s.ClientDomain == "" {
-		log.Panic("StartServer called on uninitialized structure")
+		log.Errorf("Trying to start a server with an empty initialization string")
+		return ErrBadConfig
 	}
+
 	// TLS config
 	tlsconfig := &tls.Config{
 		InsecureSkipVerify: true,
@@ -62,56 +70,89 @@ func (s *SmtpServer) StartServer() {
 	}
 	var err error
 
-	s.Client, err = smtp.Dial(s.ServerName())
+	s.Client, err = smtp.Dial(s.serverName())
 	if err != nil {
-		log.Panic(err)
+		log.Errorf("justSQL, .StartServer(): Error on dial: %v", err)
+		return err
 	}
 
 	err = s.Client.Hello(s.ClientDomain)
 	if err != nil {
-		log.Panic(err)
+		log.Errorf("justSQL, .StartServer(): Error on hello: %v", err)
+		return err
 	}
 
 	err = s.Client.StartTLS(tlsconfig)
 	if err != nil {
-		log.Panic(err)
+		log.Errorf("justSQL, .StartServer(): Error on startTLS: %v", err)
+		return err
 	}
+	return nil
 }
 
-// SendMail sends an email
-func (s *SmtpServer) SendMail(mail *Mail) {
-
+// SendMail sends an email TODO: add return error
+func (s *SmtpServer) SendMail(mail *Mail) (err error) {
 	messageBody := mail.buildMessage()
-
-	err := s.Client.Mail(mail.Sender)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	receivers := append(mail.To, mail.Cc...)
-	receivers  = append(receivers, mail.Bcc...)
-
-	for _, k := range receivers {
-		if err = s.Client.Rcpt(k); err != nil {
-			log.Panic(err)
+	var messageSent bool = false
+	var messageRetry uint8 = 3
+	defer func() {
+		if (messageRetry == 0) {
+			log.Errorf("We failed to send an email: %v!", mail)
+			err = ErrServerUnavailable
+		}
+	}()
+	handleError := func() {
+		log.Warningf("justSql, .SendMail(): Trying to restart mail server, %v-rsth time", 3-messageRetry)
+		if err := s.StartServer(); err != nil {
+			log.Errorf("justSql, .SendMail(): ServerStart error on restarted: %v", err)
+		}
+		messageRetry -= 1
+		if (messageRetry == 0) {
+			err = ErrServerUnavailable
 		}
 	}
 
-	// Data
-	w, err := s.Client.Data()
-	if err != nil {
-		log.Panic(err)
-	}
+	for ((!messageSent) && (messageRetry != 0)){
+		err := s.Client.Mail(mail.Sender)
+		if err != nil {
+			log.Errorf("justSQL, .SendMail(): Error on Mail", err)
+			handleError()
+			continue
+		}
 
-	_, err = w.Write([]byte(messageBody))
-	if err != nil {
-		log.Panic(err)
-	}
+		receivers := append(mail.To, mail.Cc...) // NOTE: receivers may or may not be pointing to mail.To
+		receivers  = append(receivers, mail.Bcc...)
 
-	err = w.Close()
-	if err != nil {
-		log.Panic(err)
+		for _, k := range receivers {
+			if err = s.Client.Rcpt(k); err != nil {
+				log.Errorf("justSQL, .SendMail: Error on Client.Rcpt", err)
+				handleError()
+				continue
+			}
+		}
+
+		// Data
+		w, err := s.Client.Data()
+		if err != nil {
+			log.Errorf("justSQL, .SendMail: Error on Data", err)
+			handleError()
+			continue
+		}
+
+		_, err = w.Write([]byte(messageBody))
+		if err != nil {
+			log.Errorf("justSQL, .SendMail: Error on Write", err)
+			handleError()
+			continue
+		}
+
+		err = w.Close()
+		if err != nil {
+			log.Errorf("justSQL, .SendMail: Error on Close", err)
+		}
+		messageSent = true
 	}
+	return nil
 }
 
 // Quit disconnects an SmtpServer
